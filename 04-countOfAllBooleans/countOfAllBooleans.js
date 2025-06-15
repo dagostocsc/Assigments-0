3ec4b77f6f22b5f11ea6b12c585c41e3d8e88f34
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-14 21:14:41 -0400

    countOfAllBooleansDone

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let result = 0;
+  arr.map((values) => {
+    if (typeof values === "boolean") {
+      result += 1;
+    }
+  });
+  return result;
 }
 
 // Do not edit this line;
