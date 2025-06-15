ee983d0d78c84158f4812e6db81b09e5f41d03f0
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-14 22:57:17 -0400

    sumOfAllOddNumbersDone

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
-function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+function sumOfAllOddNumbers(nums)
+{
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      sum ++;
+    }
+  }
+  return sum;
 }
 
 // Do not edit this line;
