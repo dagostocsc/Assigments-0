dc89378f675d8ebe7a09c3ad6136dc05a694f724
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-14 22:53:40 -0400

    sumOfAllEvenNumbersDone

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      sum ++;
+    }
+  }
+  return sum;
 }
+
+sumOfAllEvenNumbers([0,1,2,3,4,5])
+sumOfAllEvenNumbers([2,4,6,7,8,9,10])
 
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
