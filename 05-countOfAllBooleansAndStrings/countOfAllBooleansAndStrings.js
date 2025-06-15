ec02cf13b158d7864fa6708073c244b7a5d2994e
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-14 21:57:26 -0400

    countBooleansAndStringsDone

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+    if (typeof val === "boolean" || typeof val === "string") {
+      result++;
+    }
+  }
+  return result;
 }
 
 // Do not edit this line;
