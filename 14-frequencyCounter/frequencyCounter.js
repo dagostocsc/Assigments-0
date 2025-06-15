81a858193e6d07e98d9043fcbbfdd309e14141c3
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-15 17:03:15 -0400

    frequencyCounterDone

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,20 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let count = 0;
+    let frequency = {};
+
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+
+    for (let j = 0; j < word.length; j++) {
+      if (letter === word[j]) {
+        count++;
+      }
+    }
+    frequency[letter] = count;
+    count = 0;
+  }
+
+  return frequency;
 }
 
 // Do not edit this line;
