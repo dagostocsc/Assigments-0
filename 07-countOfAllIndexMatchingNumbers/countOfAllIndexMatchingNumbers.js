522e4f3c075bbd1d62dfb942307f128e30570235
Dagostocsc <Deborahagosto19@gmail.com>
2025-06-14 22:29:37 -0400

    countOfAllIndexMatchingNumbersDone

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,11 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let result = 0;
+  for (let i = 0; i< nums.length; i++) {
+    if (nums[i] === i) {
+      result++;
+    }
+  }
+  return result;
 }
 
 // Do not edit this line;
